44749323e66c70de9efc71d994b7631799c4d697
Nirmalya Choudhury <choudhurynirmalya4@gmail.com>
2024-11-12 23:10:57 +0530

    pagination

---
+++ frontend/src/Pages/Properties/All Properties/All_Properties.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const All_Properties = () => {
+  return (
+    <div>All_Properties</div>
+  )
+}
+
+export default All_Properties
---
+++ frontend/src/Pages/Properties/Top Properties/Top_Properties.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Top_Properties = () => {
+  return (
+    <div>Top_Properties</div>
+  )
+}
+
+export default Top_Properties
