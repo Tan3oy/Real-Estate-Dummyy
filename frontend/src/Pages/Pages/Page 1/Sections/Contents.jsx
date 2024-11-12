4855536e0175b8a4448bf6896c68cebd6c347934
Nirmalya Choudhury <choudhurynirmalya4@gmail.com>
2024-11-12 12:52:28 +0530

    pages

--- frontend/src/Pages/Pages/Page 1/Sections/Contents.jsx
+++ frontend/src/Pages/Pages/Page 1/Sections/Contents.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { customPagesData } from '../../../../Constants/Custom_Pages_data'
+import { customPagesData1 } from '../../../../Constants/Custom_Pages_data'
 
 const Contents = () => {
-
-  const { para1: para1_1, para2: para2_1, para3: para3_1, para4: para4_1 } = customPagesData.customPagesData1;
 
   return (
     <div className='p-28'>
       {/* Render paragraphs from customPagesData1 */}
       <div className='mx-auto shadow-[0_0_10px_0_rgba(0,0,0,0.2)] p-6 text-[#848181] leading-relaxed flex flex-col gap-5'>
-        <p>{para1_1}</p>
-        <p>{para2_1}</p>
-        <p>{para3_1}</p>
-        <p>{para4_1}</p>
+          {
+            customPagesData1.map((items, id) => (
+                <p key={id}>{items.para}</p>
+            ))
+          }
       </div>
     </div>
   )
