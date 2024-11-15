77dadb96bb0097d12962a00ea905c81a59e8447c
Nirmalya Choudhury <choudhurynirmalya4@gmail.com>
2024-11-15 13:14:02 +0530

    backend set1

--- backend/Models/FAQ_data.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const faqSchema= new mongoose.Schema({
-    question: String,
-    answer: String,
-    required: true
-})
-
-export default mongoose.model("FAQ", faqSchema)
--- backend/Routes/FAQ_Routes.js
+++
@@ -1,6 +0,0 @@
-import express from  "express"
-import {FAQ_Router} from "../Controllers/FAQ_Controller"
-
-const router = express.Router()
-
-router.post("/")
