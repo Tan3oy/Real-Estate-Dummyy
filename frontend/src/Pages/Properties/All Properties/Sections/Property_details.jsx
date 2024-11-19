b7478ae5a9cac2f44611135fae120479c11b15f3
Nirmalya Choudhury <choudhurynirmalya4@gmail.com>
2024-11-19 15:03:24 +0530

    arup solve

--- frontend/src/Pages/Properties/All Properties/Sections/Property_details.jsx
+++ frontend/src/Pages/Properties/All Properties/Sections/Property_details.jsx
@@ -7,6 +7,8 @@
     import { FaEye } from "react-icons/fa";
     import { FaCommentDots } from "react-icons/fa";
     import { MdDone } from "react-icons/md";
+    import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+    import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
     const Property_details = () => {
@@ -21,29 +23,40 @@
                         <h2 className='text-xl font-medium text-white text-center'>Home - Properties -{items.speciality}</h2>
                     </div>      
                     </section>
-                    <div className="flex justify-between">
-                        <div className="property_contact p-4 w-[40%] h-fit border shadow-[0px_0px_8px_0px_#97999db8] ">
-                            <div className="contractor_details">John Doe</div>
-                            <form className='flex flex-col gap-3'>
+                    <div className="mx-32 flex justify-evenly gap-6">
+                        <div className="property_contact p-4 w-[32%] h-fit border shadow-[0px_0px_8px_0px_#97999db8] ">
+                            <div className="contractor_details flex justify-start gap-4 items-center bg-green-600 text-white font-bold p-4 rounded-t-xl mb-7">
+                                <div>
+                                    <img src="https://demo.websolutionus.com/findestate/uploads/website-images/earl-newell-2021-10-07-08-20-15-5266.jpg" alt='John Doe Image' className='w-[72px] h-fit rounded-[50%]' />
+                                </div>
+                                <div>
+                                    <p className='text-xl font-bold'>John Doe</p>
+                                    <div className='flex items-center gap-1'>
+                                        <FontAwesomeIcon icon={faEnvelope} />
+                                        <p className='text-[18px] font-normal align-bottom'>admin@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <form className='flex flex-col gap-5'>
                                 <div className=" flex flex-col gap-2">
-                                    <p>Name</p>
+                                    <p className='text-[#093B55] font-medium'>Name</p>
                                     <input type="text" className='p-4 border outline-none border-[#919191] w-full roubded-md' />
                                 </div>
                                 <div className=" flex flex-col gap-2">
-                                    <p>Email</p>
+                                    <p className='text-[#093B55] font-medium'>Email</p>
                                     <input type="text" className='p-4 border outline-none border-[#919191] w-full roubded-md' />
                                 </div>
                                 <div className=" flex flex-col gap-2">
-                                    <p>Phone</p>
+                                    <p className='text-[#093B55] font-medium'>Phone</p>
                                     <input type="text" className='p-4 border outline-none border-[#919191] w-full roubded-md' />
                                 </div>
                                 <div className=" flex flex-col gap-2">
-                                    <p>Description</p>
+                                    <p className='text-[#093B55] font-medium'>Description</p>
                                     <textarea name="" id="" rows="10" className='border outline-none border-[#919191] roubded-md h-24 w-full'></textarea>
                                 </div>                               
                             </form>
                         </div>
-                        <div className="Property-details p-4 w-[58%] h-fit border shadow-[0px_0px_8px_0px_#97999db8]">
+                        <div className="Property-details p-4 w-[68%] h-fit border shadow-[0px_0px_8px_0px_#97999db8]">
                             <div className="propert-intro">
                                 <div className="property-labels flex gap-3">
                                     <div className="flex items-center gap-3">
