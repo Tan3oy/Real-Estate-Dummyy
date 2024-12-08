3141ee942fc98d150a0c8dfdfb98cfb2fc7e1334
Tan3oy <tanmoy122halder@gmail.com>
2024-12-08 23:50:25 +0530

    Fixed Some Bugs

--- frontend/src/Components/BreadCrumbs.jsx
+++ frontend/src/Components/BreadCrumbs.jsx
@@ -7,26 +7,23 @@ const BreadCrumbs = () => {
     const paths2=paths.filter((el)=>(
         paths.indexOf(el)!==paths.length-1
     ))
-    const paths3=paths2.map((el)=>el.replace(/_/, ' '))
     console.log("Path Arr",paths);
     console.log("Path Arr2",paths2);
-    console.log("Path Arr3",paths3);
     console.log(routePath);
     let BreadCrumbPath=""
-    // console.log("BreadCrumbPath",BreadCrumbPath);
     
     
   return (
     <div className='flex'>
         <p><Link to="/" className='text-[8px] md:text-sm lg:text-lg'>Home</Link></p>
       {
-            paths3.map((el,i)=>{
+            paths2.map((el,i)=>{
               BreadCrumbPath+=`/${el}`
               console.log("BreadCrumbPath",BreadCrumbPath);
               
                 return <span key={i}>
                   <Link to={BreadCrumbPath}>               
-                    <span className='capitalize text-[8px] md:text-sm lg:text-lg'>&nbsp;-&nbsp;{el}</span>
+                    <span className='capitalize text-[8px] md:text-sm lg:text-lg'>&nbsp;-&nbsp;{el.replace(/_/g," ")}</span>
                   </Link>
                   {/* <span className=''>&nbsp;-&nbsp;</span> */}
                 </span>               
